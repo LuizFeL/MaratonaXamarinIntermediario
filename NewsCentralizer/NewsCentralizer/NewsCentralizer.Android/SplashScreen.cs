865dfe180b113cbbb39a9b91c02edfbb860f174e
lfsantos <lfsantos@processwaregroup.com>
2017-05-25 16:27:34 -0300

    Start work

---
+++ NewsCentralizer/NewsCentralizer/NewsCentralizer.Android/SplashScreen.cs
@@ -0,0 +1,20 @@
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+
+namespace NewsCentralizer.Droid
+{
+    [Activity(Label = "FeL - News Centralizer", Icon = "@drawable/icon", MainLauncher = true, NoHistory = true, Theme = "@style/Theme.Splash", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            var intent = new Intent(this, typeof(MainActivity));
+            StartActivity(intent);
+            Finish();
+        }
+    }
+}
